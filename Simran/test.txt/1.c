0195d2b46bbe3bb6a2de668e3b67fcdff6525398
SimranTaj-sim <simrantajapsanataj@gmail.com>
2019-09-21 21:30:43 +0530

    update

---
+++ Simran/test.txt/1.c
@@ -0,0 +1,14 @@
+1.store elements in an array 
+
+	#include<stdio.h>
+	int main()
+	{
+
+ 	 int arr[10],i;
+	 for(i=0;i<10;i++)
+ 	 {
+   	  printf("enter the elements %d\n",i);
+   	  scanf("%d\n",&arr[i]);
+	 }
+  
+        }
