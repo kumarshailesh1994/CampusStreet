5e51d514f1d2b81d0a7f19866f249b9623451f39
ratneshkumar1166 <55450746+ratneshkumar1166@users.noreply.github.com>
2019-09-22 14:38:58 +0530

    Update addition 2d matrix.c

--- Ratnesh/week 2 assignment 1 by ratnesh/addition 2d matrix.c
+++ Ratnesh/week 2 assignment 1 by ratnesh/addition 2d matrix.c
@@ -8,16 +8,16 @@ int main()
    scanf("%d%d", &m, &n);
    printf("Enter the elements of first matrix\n");
  
-   for (c = 0; c < m; c++)
-      for (d = 0; d < n; d++)
+   for (c = 0; c < m; c++){
+      for (d = 0; d < n; d++){
          scanf("%d", &first[c][d]);
- 
+      }}
    printf("Enter the elements of second matrix\n");
  
-   for (c = 0; c < m; c++)
-      for (d = 0 ; d < n; d++)
+   for (c = 0; c < m; c++){
+      for (d = 0 ; d < n; d++){
          scanf("%d", &second[c][d]);
-   
+      }}
    printf("Sum of entered matrices:-\n");
    
    for (c = 0; c < m; c++) {
